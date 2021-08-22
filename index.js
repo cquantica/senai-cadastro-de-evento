42177df852d7811d2426aa3b5f0de85db265df6a
Hyasmym <coachquantica@gmail.com>
2021-08-22 17:41:12 -0300

    Atualização Cadastro de eventos

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-    let nomeEvento = "Evento Teste";
+    let nomeEvento = "Digite o nome do seu evento";
     console.log(nomeEvento);
     let data = "22/08/2021";
     if (data == "21/08/2021") {
         console.log("O cadastro não pode ser efetuado, pois a data é inválida.");
     }
    let participantes = ["Alice Valey", "Bruno Bernardo", "Camilo Borges"];
-   console.log("Exibir a lista de participantes.");
+   console.log("Lista de participantes.");
 
    let numeroDeParticipantes = participantes.length;
    console.log(nomeEvento);
@@ -15,7 +15,7 @@
    console.log("Digite sua idade");
    let idade = 18;
    if (idade >=18) {
-       console.log("Digite seu nome");
+       console.log("Digite seu email");
    }
     else {
        console.log("Voce não possui idade suficiente para participar do evento");
@@ -27,8 +27,9 @@
         console.log("Limite de participante excedido!")
            
        }
-       console.log("Estes serão nossos Palestrantes:")  
-          
+      
+      console.log("Estes serão nossos Palestrantes:")  
+
     let listaDePalestrante = ["Arthur Alma", "Joana Spirit", "Leon Java"];
     let quantidadeDePalestrante = listaDePalestrante.length;
     for (let indice=0; indice < quantidadeDePalestrante; indice++) {
